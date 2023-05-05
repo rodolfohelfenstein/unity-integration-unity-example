afd8be4d8c9a707d48ba31aebc1e0c2eebb2ca0c
Rodolfo Helfenstein Bulgam <rodolfo.helfenstein@MMB031152CPS.local>
2023-05-05 13:31:30 -0300

    Initial Commit

---
+++ UnityIntegrationHUB/Assets/Scripts/Initializer.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityIntegrationHUB
+{
+
+    public class Initializer : MonoBehaviour
+    {
+
+        private void Start()
+        { }
+
+    }
+
+}
